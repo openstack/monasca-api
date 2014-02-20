6922c958aa1d615f576487f8f3799c7ae7b580af
Jonathan Halterman <jhalterman@gmail.com>
2014-02-19 21:43:49 -0800

    Added some domain model tests

---
+++ src/test/java/com/hpcloud/mon/domain/model/AlarmTest.java
@@ -0,0 +1,42 @@
+package com.hpcloud.mon.domain.model;
+
+import static com.hpcloud.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.testng.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
+import com.hpcloud.mon.common.model.alarm.AlarmState;
+import com.hpcloud.mon.domain.model.alarm.AlarmDetail;
+import com.hpcloud.mon.domain.model.common.Link;
+
+@Test
+public class AlarmTest extends AbstractModelTest {
+  private final AlarmDetail alarm;
+  private final Map<String, String> dimensions;
+
+  public AlarmTest() {
+    dimensions = new HashMap<String, String>();
+    dimensions.put("instance_id", "666");
+    dimensions.put("image_id", "345");
+    alarm = new AlarmDetail("123", "90% CPU",
+        "avg(hpcs.compute:cpu:1:{instance_id=666, image_id=345}) >= 90", AlarmState.OK, Arrays.asList(
+            "123345345", "23423"));
+    alarm.setLinks(Arrays.asList(new Link("self", "https://region-a.geo-1.maas.hpcloudsvc.com/v1.0")));
+  }
+
+  public void shouldSerializeToJson() throws Exception {
+    String json = toJson(alarm);
+    assertEquals(json, jsonFixture("fixtures/alarm.json"));
+  }
+
+  public void shouldDeserializeFromJson() throws Exception {
+    String json = jsonFixture("fixtures/alarm.json");
+    AlarmDetail detail = fromJson(json, AlarmDetail.class);
+    assertEquals(alarm, detail);
+    assertEquals(alarm.getAlarmActions(), detail.getAlarmActions());
+  }
+}
---
+++ src/test/java/com/hpcloud/mon/domain/model/NotificationMethodTest.java
@@ -0,0 +1,35 @@
+package com.hpcloud.mon.domain.model;
+
+import static com.hpcloud.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.testng.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+import com.hpcloud.mon.domain.model.common.Link;
+import com.hpcloud.mon.domain.model.notificationmethod.NotificationMethod;
+import com.hpcloud.mon.domain.model.notificationmethod.NotificationMethod.NotificationMethodType;
+
+@Test
+public class NotificationMethodTest extends AbstractModelTest {
+  private final NotificationMethod notificationMethod;
+
+  public NotificationMethodTest() {
+    notificationMethod = new NotificationMethod("123", "MySMS", NotificationMethodType.SMS,
+        "9228675309");
+    notificationMethod.setLinks(Arrays.asList(new Link("self",
+        "https://region-a.geo-1.maas.hpcloudsvc.com/v1.0")));
+  }
+
+  public void shouldSerializeToJson() throws Exception {
+    String json = toJson(notificationMethod);
+    assertEquals(json, jsonFixture("fixtures/notificationMethod.json"));
+  }
+
+  public void shouldDeserializeFromJson() throws Exception {
+    String json = jsonFixture("fixtures/notificationMethod.json");
+    NotificationMethod detail = fromJson(json, NotificationMethod.class);
+    assertEquals(notificationMethod, detail);
+  }
+}
