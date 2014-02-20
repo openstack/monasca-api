7a6d7760c879e7f0e58d07f75d303804896f057a
Jonathan Halterman <jhalterman@gmail.com>
2014-02-19 21:05:32 -0800

    Removed Zookeeper and LockService for now

--- src/main/java/com/hpcloud/mon/infrastructure/InfrastructureModule.java
+++ src/main/java/com/hpcloud/mon/infrastructure/InfrastructureModule.java
@@ -1,11 +1,7 @@
 package com.hpcloud.mon.infrastructure;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Scopes;
 import com.hpcloud.mon.MonApiConfiguration;
-import com.hpcloud.mon.domain.service.LockService;
-import com.hpcloud.mon.infrastructure.zookeeper.ZookeeperConfiguration;
-import com.hpcloud.mon.infrastructure.zookeeper.ZookeeperLockService;
 
 /**
  * Infrastructure layer bindings.
@@ -21,22 +17,20 @@ public class InfrastructureModule extends AbstractModule {
 
   @Override
   protected void configure() {
-//    bind(AccessRepository.class).to(AccessRepositoryImpl.class).in(Scopes.SINGLETON);
-//    bind(DatapointRepository.class).to(SomDatapointRepositoryImpl.class).in(Scopes.SINGLETON);
-//    bind(EndpointRepository.class).to(EndpointRepositoryImpl.class).in(Scopes.SINGLETON);
-//    bind(SubscriptionRepository.class).to(SubscriptionRepositoryImpl.class).in(Scopes.SINGLETON);
-//    bind(NotificationMethodRepository.class).to(NotificationMethodRepositoryImpl.class).in(
-//        Scopes.SINGLETON);
-//    bind(AlarmRepository.class).to(AlarmRepositoryImpl.class).in(Scopes.SINGLETON);
-//    bind(IdentityServiceClient.class).in(Scopes.SINGLETON);
-//    bind(ResourceVerificationService.class).annotatedWith(Names.named(Namespaces.COMPUTE_NAMESPACE))
-//        .to(ComputeResourceVerificationService.class)
-//        .in(Scopes.SINGLETON);
-//    bind(ResourceVerificationService.class).annotatedWith(
-//        Names.named(Namespaces.OBJECT_STORE_NAMESPACE))
-//        .to(ObjectStoreResourceVerificationService.class)
-//        .in(Scopes.SINGLETON);
-    bind(ZookeeperConfiguration.class).toInstance(config.zookeeper);
-    bind(LockService.class).to(ZookeeperLockService.class).in(Scopes.SINGLETON);
+    // bind(AccessRepository.class).to(AccessRepositoryImpl.class).in(Scopes.SINGLETON);
+    // bind(DatapointRepository.class).to(SomDatapointRepositoryImpl.class).in(Scopes.SINGLETON);
+    // bind(EndpointRepository.class).to(EndpointRepositoryImpl.class).in(Scopes.SINGLETON);
+    // bind(SubscriptionRepository.class).to(SubscriptionRepositoryImpl.class).in(Scopes.SINGLETON);
+    // bind(NotificationMethodRepository.class).to(NotificationMethodRepositoryImpl.class).in(
+    // Scopes.SINGLETON);
+    // bind(AlarmRepository.class).to(AlarmRepositoryImpl.class).in(Scopes.SINGLETON);
+    // bind(IdentityServiceClient.class).in(Scopes.SINGLETON);
+    // bind(ResourceVerificationService.class).annotatedWith(Names.named(Namespaces.COMPUTE_NAMESPACE))
+    // .to(ComputeResourceVerificationService.class)
+    // .in(Scopes.SINGLETON);
+    // bind(ResourceVerificationService.class).annotatedWith(
+    // Names.named(Namespaces.OBJECT_STORE_NAMESPACE))
+    // .to(ObjectStoreResourceVerificationService.class)
+    // .in(Scopes.SINGLETON);
   }
 }
