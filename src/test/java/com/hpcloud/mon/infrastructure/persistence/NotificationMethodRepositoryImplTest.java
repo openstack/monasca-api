c5ea50d9a9dd1d99f36b18134403c71bf024f6dd
Jonathan Halterman <jhalterman@gmail.com>
2014-02-21 15:41:52 -0800

    Add repository test implementations

---
+++ src/test/java/com/hpcloud/mon/infrastructure/persistence/NotificationMethodRepositoryImplTest.java
@@ -0,0 +1,91 @@
+package com.hpcloud.mon.infrastructure.persistence;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.google.common.io.Resources;
+import com.hpcloud.mon.domain.exception.EntityNotFoundException;
+import com.hpcloud.mon.domain.model.notificationmethod.NotificationMethod;
+import com.hpcloud.mon.domain.model.notificationmethod.NotificationMethod.NotificationMethodType;
+
+/**
+ * @author Jonathan Halterman
+ */
+@Test
+public class NotificationMethodRepositoryImplTest {
+  private DBI db;
+  private Handle handle;
+  private NotificationMethodRepositoryImpl repo;
+
+  @BeforeClass
+  protected void beforeClass() throws Exception {
+    db = new DBI("jdbc:h2:mem:test;MODE=MySQL");
+    handle = db.open();
+    handle.execute(Resources.toString(getClass().getResource("notification_method.sql"),
+        Charset.defaultCharset()));
+    repo = new NotificationMethodRepositoryImpl(db);
+  }
+
+  @AfterClass
+  protected void afterClass() {
+    handle.close();
+  }
+
+  @BeforeMethod
+  protected void beforeMethod() {
+    handle.execute("truncate table notification_method");
+    handle.execute("insert into notification_method (id, tenant_id, name, type, address, created_at, updated_at) values ('123', '444', 'MySMS', 'SMS', '8675309', NOW(), NOW())");
+  }
+
+  public void shouldCreate() {
+    NotificationMethod nmA = repo.create("555", "MySMS", NotificationMethodType.SMS, "5555555");
+    NotificationMethod nmB = repo.findById("555", nmA.getId());
+
+    assertEquals(nmA, nmB);
+  }
+
+  public void shouldExistForTenantAndNotificationMethod() {
+    assertTrue(repo.exists("444", "123"));
+    assertFalse(repo.exists("444", "1234"));
+    assertFalse(repo.exists("333", "123"));
+  }
+
+  public void shouldFindById() {
+    NotificationMethod nm = repo.findById("444", "123");
+
+    assertEquals(nm.getId(), "123");
+    assertEquals(nm.getType(), NotificationMethodType.SMS);
+    assertEquals(nm.getAddress(), "8675309");
+  }
+
+  public void shouldFind() {
+    List<NotificationMethod> nms = repo.find("444");
+
+    assertEquals(
+        nms,
+        Arrays.asList(new NotificationMethod("123", "MySMS", NotificationMethodType.SMS, "8675309")));
+  }
+
+  public void shouldDeleteById() {
+    repo.deleteById("444", "123");
+
+    try {
+      repo.findById("444", "123");
+      fail();
+    } catch (EntityNotFoundException expected) {
+    }
+  }
+}
---
+++ src/test/resources/com/hpcloud/mon/infrastructure/persistence/alarm.sql
@@ -0,0 +1,39 @@
+CREATE TABLE `alarm` (
+  `id` varchar(36) NOT NULL,
+  `tenant_id` varchar(36) NOT NULL,
+  `name` varchar(250) NOT NULL,
+  `expression` mediumtext NOT NULL,
+  `state` varchar(20) NOT NULL check state in ('UNDETERMINED','OK','ALARM'),
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `sub_alarm` (
+  `id` varchar(36) NOT NULL,
+  `alarm_id` varchar(36) NOT NULL,
+  `function` varchar(10) NOT NULL,
+  `namespace` varchar(100) NOT NULL,
+  `operator` varchar(5) NOT NULL,
+  `threshold` double NOT NULL,
+  `period` int(11) NOT NULL,
+  `periods` int(11) NOT NULL,
+  `state` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `sub_alarm_dimension` (
+  `sub_alarm_id` varchar(36) NOT NULL,
+  `dimension_name` varchar(50) NOT NULL,
+  `value` varchar(300) NOT NULL,
+  PRIMARY KEY (`sub_alarm_id`,`dimension_name`)
+);
+
+CREATE TABLE `alarm_action` (
+  `alarm_id` varchar(36) NOT NULL,
+  `action_id` varchar(50) NOT NULL,
+  PRIMARY KEY (`alarm_id`,`action_id`)
+);
