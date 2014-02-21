811074907a69fdc62bb6ee0b1848aa5d91032922
Jonathan Halterman <jhalterman@gmail.com>
2014-02-21 15:52:50 -0800

    Committing initial disabled metric and alarm integration tests

---
+++ src/test/java/com/hpcloud/mon/integration/AlarmIntegrationTest.java
@@ -0,0 +1,155 @@
+package com.hpcloud.mon.integration;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.fail;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import kafka.javaapi.producer.Producer;
+
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.google.common.io.Resources;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.hpcloud.mon.MonApiConfiguration;
+import com.hpcloud.mon.MonApiModule;
+import com.hpcloud.mon.app.AlarmService;
+import com.hpcloud.mon.app.command.CreateAlarmCommand;
+import com.hpcloud.mon.common.model.alarm.AlarmState;
+import com.hpcloud.mon.domain.exception.EntityNotFoundException;
+import com.hpcloud.mon.domain.model.alarm.Alarm;
+import com.hpcloud.mon.domain.model.alarm.AlarmDetail;
+import com.hpcloud.mon.domain.model.alarm.AlarmRepository;
+import com.hpcloud.mon.infrastructure.persistence.AlarmRepositoryImpl;
+import com.hpcloud.mon.infrastructure.persistence.NotificationMethodRepositoryImpl;
+import com.hpcloud.mon.resource.AbstractMonApiResourceTest;
+import com.hpcloud.mon.resource.AlarmResource;
+import com.sun.jersey.api.client.ClientResponse;
+
+/**
+ * @author Jonathan Halterman
+ */
+@Test(groups = "integration", enabled = false)
+public class AlarmIntegrationTest extends AbstractMonApiResourceTest {
+  private static final String TENANT_ID = "alarm-test";
+  private DBI db;
+  private AlarmDetail alarm;
+  private AlarmService service;
+  private MonApiConfiguration config;
+  private Producer<String, String> producer;
+  private AlarmRepository repo;
+  private Map<String, String> dimensions;
+  private List<String> alarmActions;
+
+  @Override
+  protected void setupResources() throws Exception {
+    super.setupResources();
+    Handle handle = db.open();
+    handle.execute("truncate table alarm");
+    handle.execute("truncate table notification_method");
+    handle.execute("insert into notification_method (id, tenant_id, name, type, address, created_at, updated_at) values ('29387234', 'alarm-test', 'MySMS', 'SMS', '8675309', NOW(), NOW())");
+    handle.execute("insert into notification_method (id, tenant_id, name, type, address, created_at, updated_at) values ('77778687', 'alarm-test', 'MySMS', 'SMS', '8675309', NOW(), NOW())");
+    db.close(handle);
+
+    repo = new AlarmRepositoryImpl(db);
+    service = new AlarmService(config, producer, repo, new NotificationMethodRepositoryImpl(db));
+    addResources(new AlarmResource(service, repo));
+  }
+
+  @BeforeTest
+  protected void beforeTest() throws Exception {
+    config = getConfiguration("config-test.yml", MonApiConfiguration.class);
+    Injector injector = Guice.createInjector(new MonApiModule(environment, config));
+    producer = injector.getInstance(Key.get(new TypeLiteral<Producer<String, String>>() {
+    }));
+    db = injector.getInstance(DBI.class);
+    Handle handle = db.open();
+    handle.execute(Resources.toString(
+        NotificationMethodRepositoryImpl.class.getResource("alarm.sql"), Charset.defaultCharset()));
+    handle.execute(Resources.toString(
+        NotificationMethodRepositoryImpl.class.getResource("notification_method.sql"),
+        Charset.defaultCharset()));
+    handle.close();
+
+    // Fixtures
+    dimensions = new HashMap<String, String>();
+    dimensions.put("instance_id", "937");
+    alarmActions = new ArrayList<String>();
+    alarmActions.add("29387234");
+    alarmActions.add("77778687");
+    alarm = new AlarmDetail("123", "90% CPU", "avg(hpcs.compute:cpu:{instance_id=123} > 10",
+        AlarmState.OK, alarmActions);
+  }
+
+  @AfterTest
+  protected void afterTest() throws Exception {
+    producer.close();
+  }
+
+  public void shouldCreate() throws Exception {
+    ClientResponse response = client().resource("/v2.0/alarms")
+        .header("X-Tenant-Id", TENANT_ID)
+        .header("Content-Type", MediaType.APPLICATION_JSON)
+        .post(
+            ClientResponse.class,
+            new CreateAlarmCommand("90% CPU", "avg(hpcs.compute:cpu:{instance_id=123} > 10",
+                alarmActions));
+
+    AlarmDetail newAlarm = response.getEntity(AlarmDetail.class);
+    String location = response.getHeaders().get("Location").get(0);
+    assertEquals(response.getStatus(), 201);
+    assertEquals(location, "/v2.0/alarms/" + newAlarm.getId());
+    assertEquals(alarm.getExpression(), newAlarm.getExpression());
+    assertEquals(alarm.getAlarmActions(), newAlarm.getAlarmActions());
+  }
+
+  public void shouldCreateCaseInsensitiveAndKeywords() throws Exception {
+    AlarmDetail alarm_local;
+    alarm_local = new AlarmDetail("123", "90% CPU", "AvG(avg:cpu:{instance_id=123} gT 10",
+        AlarmState.OK, alarmActions);
+    ClientResponse response = client().resource("/v2.0/alarms")
+        .header("X-Tenant-Id", TENANT_ID)
+        .header("Content-Type", MediaType.APPLICATION_JSON)
+        .post(ClientResponse.class,
+            new CreateAlarmCommand("90% CPU", "AvG(avg:cpu:{instance_id=123} gT 10", alarmActions));
+
+    AlarmDetail newAlarm = response.getEntity(AlarmDetail.class);
+    String location = response.getHeaders().get("Location").get(0);
+    assertEquals(response.getStatus(), 201);
+    assertEquals(location, "/v2.0/alarms/" + newAlarm.getId());
+    assertEquals(alarm_local.getExpression(), newAlarm.getExpression());
+    assertEquals(alarm_local.getAlarmActions(), newAlarm.getAlarmActions());
+  }
+
+  public void shouldDelete() {
+    Alarm newAlarm = repo.create("123", TENANT_ID, alarm.getName(), alarm.getExpression(), null,
+        alarm.getAlarmActions());
+    assertNotNull(repo.findById(TENANT_ID, newAlarm.getId()));
+
+    ClientResponse response = client().resource("/v2.0/alarms/" + newAlarm.getId())
+        .header("X-Tenant-Id", TENANT_ID)
+        .delete(ClientResponse.class);
+    assertEquals(response.getStatus(), 204);
+
+    try {
+      assertNull(repo.findById(TENANT_ID, newAlarm.getId()));
+      fail();
+    } catch (EntityNotFoundException expected) {
+    }
+  }
+}
---
+++ src/test/java/com/hpcloud/mon/integration/MetricIntegrationTest.java
@@ -0,0 +1,100 @@
+package com.hpcloud.mon.integration;
+
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import kafka.javaapi.producer.Producer;
+
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.hpcloud.mon.MonApiConfiguration;
+import com.hpcloud.mon.MonApiModule;
+import com.hpcloud.mon.app.MetricService;
+import com.hpcloud.mon.app.command.CreateMetricCommand;
+import com.hpcloud.mon.domain.model.metric.DatapointRepository;
+import com.hpcloud.mon.resource.AbstractMonApiResourceTest;
+import com.hpcloud.mon.resource.MetricResource;
+import com.sun.jersey.api.client.ClientResponse;
+
+/**
+ * @author Todd Walk
+ */
+@Test(groups = "integration", enabled = false)
+public class MetricIntegrationTest extends AbstractMonApiResourceTest {
+  private static final String TENANT_ID = "metric-test";
+  private DBI db;
+  private MetricService service;
+  private Producer<String, String> producer;
+  private MonApiConfiguration config;
+  private DatapointRepository datapointRepo;
+  private Map<String, String> dimensions;
+
+  @Override
+  protected void setupResources() throws Exception {
+    super.setupResources();
+    Handle handle = db.open();
+    handle.execute("truncate table access");
+    db.close(handle);
+    datapointRepo = mock(DatapointRepository.class);
+    service = new MetricService(config, producer, metricRegistry);
+    addResources(new MetricResource(service, datapointRepo));
+  }
+
+  @BeforeTest
+  protected void beforeTest() throws Exception {
+    config = getConfiguration("config-test.yml", MonApiConfiguration.class);
+    Injector injector = Guice.createInjector(new MonApiModule(environment, config));
+    producer = injector.getInstance(Key.get(new TypeLiteral<Producer<String, String>>() {
+    }));
+  }
+
+  @AfterTest
+  protected void afterTest() throws Exception {
+    producer.close();
+  }
+
+  public void shouldCreate() throws Exception {
+    dimensions = new HashMap<String, String>();
+    dimensions.put("instance_id", "937");
+    dimensions.put("az", "2");
+    dimensions.put("instance_uuid", "abc123");
+    long timestamp = System.currentTimeMillis() / 1000;
+    ClientResponse response = client().resource("/v2.0/metrics")
+        .header("X-Tenant-Id", TENANT_ID)
+        .header("Content-Type", MediaType.APPLICATION_JSON)
+        .post(ClientResponse.class,
+            new CreateMetricCommand("test_namespace", dimensions, timestamp, 22.0));
+
+    assertEquals(response.getStatus(), 204);
+  }
+
+  public void shouldCreateList() throws Exception {
+    dimensions = new HashMap<String, String>();
+    dimensions.put("instance_id", "937");
+    dimensions.put("az", "2");
+    dimensions.put("instance_uuid", "abc123");
+    long timestamp = System.currentTimeMillis() / 1000;
+    double timestampD = (double) timestamp;
+    double[][] timeValues = { { timestampD, 22.0 }, { timestampD + 1, 23.0 } };
+    ClientResponse response = client().resource("/v2.0/metrics")
+        .header("X-Tenant-Id", TENANT_ID)
+        .header("Content-Type", MediaType.APPLICATION_JSON)
+        .post(ClientResponse.class,
+            new CreateMetricCommand("test_namespace", dimensions, timestamp, timeValues));
+
+    assertEquals(response.getStatus(), 204);
+  }
+}
