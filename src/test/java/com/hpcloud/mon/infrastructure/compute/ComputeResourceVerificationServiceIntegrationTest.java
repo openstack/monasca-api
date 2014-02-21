780a377f77c08659842a91eb113698bf82e35178
Jonathan Halterman <jhalterman@gmail.com>
2014-02-20 17:41:28 -0800

    Removed secondary resource IDs from compute resource verification service (Bravo FTW)!

--- src/test/java/com/hpcloud/mon/infrastructure/compute/ComputeResourceVerificationServiceIntegrationTest.java
+++ src/test/java/com/hpcloud/mon/infrastructure/compute/ComputeResourceVerificationServiceIntegrationTest.java
@@ -58,29 +58,27 @@ public class ComputeResourceVerificationServiceIntegrationTest {
   public void shouldVerifyOwnerOfResource() {
     ComputeResourceVerificationService service = serviceFor("1.1");
     assertTrue(service.isVerifiedOwner("46995959297574", "d912fa79-77c1-40e0-8794-53829909d7b6",
-        "2073779", "1", null));
+        "1", null));
   }
 
   @Test(expectedExceptions = ResourceNotFoundException.class)
   public void shouldRejectNonExistentResources() {
     ComputeResourceVerificationService service = serviceFor("1.1");
-    service.isVerifiedOwner("46995959297574", "d912fa79-77c1-40e0-8794-538299091234", "2073779",
-        "2", null);
+    service.isVerifiedOwner("46995959297574", "d912fa79-77c1-40e0-8794-538299091234", "2", null);
   }
 
   // Wrong instance id
   @Test(expectedExceptions = IllegalArgumentException.class)
   public void shouldRejectMismatchedServiceIds() {
     ComputeResourceVerificationService service = serviceFor("1.1");
-    service.isVerifiedOwner("46995959297574", "d912fa79-77c1-40e0-8794-53829909d7b6", "222233",
-        "1", null);
+    service.isVerifiedOwner("46995959297574", "d912fa79-77c1-40e0-8794-53829909d7b6", "1", null);
   }
 
   // Wrong tenant
   public void shouldRejectNonOwner() {
     ComputeResourceVerificationService service = serviceFor("1.1");
     assertFalse(service.isVerifiedOwner("78129159605336", "d912fa79-77c1-40e0-8794-53829909d7b6",
-        "2073779", "1", null));
+        "1", null));
   }
 
   /**
@@ -89,12 +87,12 @@ public class ComputeResourceVerificationServiceIntegrationTest {
   public void shouldVerifyOwnerOfResourceInBravo() {
     ComputeResourceVerificationService service = serviceFor("2");
     assertTrue(service.isVerifiedOwner("46995959297574", "d770e4cc-10db-4717-a36d-29c10d4a0b5e",
-        "d770e4cc-10db-4717-a36d-29c10d4a0b5e", null, null));
+        null, null));
   }
 
   public void shouldRejectNonOwnerInBravo() {
     ComputeResourceVerificationService service = serviceFor("2");
     assertFalse(service.isVerifiedOwner("78129159605336", "d770e4cc-10db-4717-a36d-29c10d4a0b5e",
-        "d770e4cc-10db-4717-a36d-29c10d4a0b5e", null, null));
+        null, null));
   }
 }
