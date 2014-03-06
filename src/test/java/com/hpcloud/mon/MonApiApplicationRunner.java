c5d33c0c905e1c7e148239c0d534bac8d09e893c
Jonathan Halterman <jhalterman@gmail.com>
2014-03-06 15:36:46 -0800

    Point to proper test config file

--- src/test/java/com/hpcloud/mon/MonApiApplicationRunner.java
+++ src/test/java/com/hpcloud/mon/MonApiApplicationRunner.java
@@ -2,6 +2,6 @@ package com.hpcloud.mon;
 
 public class MonApiApplicationRunner {
   public static void main(String... args) throws Exception {
-    MonApiApplication.main(new String[] { "server", "config-test.yml" });
+    MonApiApplication.main(new String[] { "server", "config-local.yml" });
   }
 }
