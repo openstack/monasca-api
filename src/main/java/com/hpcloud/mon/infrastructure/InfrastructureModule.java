1333c9f09f71f4832d2f4de9d1597744ac6de998
Jonathan Halterman <jhalterman@gmail.com>
2014-03-21 14:35:25 -0700

    Removed resource ownership verification service and implementations. Removed identity service implementations.

--- src/main/java/com/hpcloud/mon/infrastructure/InfrastructureModule.java
+++ src/main/java/com/hpcloud/mon/infrastructure/InfrastructureModule.java
@@ -3,16 +3,10 @@ package com.hpcloud.mon.infrastructure;
 import javax.inject.Singleton;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.name.Names;
-import com.hpcloud.mon.common.model.Services;
 import com.hpcloud.mon.domain.model.alarm.AlarmRepository;
 import com.hpcloud.mon.domain.model.measurement.MeasurementRepository;
 import com.hpcloud.mon.domain.model.metric.MetricRepository;
 import com.hpcloud.mon.domain.model.notificationmethod.NotificationMethodRepository;
-import com.hpcloud.mon.domain.service.ResourceVerificationService;
-import com.hpcloud.mon.infrastructure.compute.ComputeResourceVerificationService;
-import com.hpcloud.mon.infrastructure.identity.IdentityServiceClient;
-import com.hpcloud.mon.infrastructure.objectstore.ObjectStoreResourceVerificationService;
 import com.hpcloud.mon.infrastructure.persistence.AlarmRepositoryImpl;
 import com.hpcloud.mon.infrastructure.persistence.MeasurementRepositoryImpl;
 import com.hpcloud.mon.infrastructure.persistence.MetricRepositoryImpl;
@@ -32,15 +26,5 @@ public class InfrastructureModule extends AbstractModule {
     bind(MeasurementRepository.class).to(MeasurementRepositoryImpl.class).in(Singleton.class);
     bind(NotificationMethodRepository.class).to(NotificationMethodRepositoryImpl.class).in(
         Singleton.class);
-
-    // Bind domain services
-    bind(IdentityServiceClient.class).in(Singleton.class);
-    bind(ResourceVerificationService.class).annotatedWith(Names.named(Services.COMPUTE_SERVICE))
-        .to(ComputeResourceVerificationService.class)
-        .in(Singleton.class);
-    bind(ResourceVerificationService.class).annotatedWith(
-        Names.named(Services.OBJECT_STORE_SERVICE))
-        .to(ObjectStoreResourceVerificationService.class)
-        .in(Singleton.class);
   }
 }
