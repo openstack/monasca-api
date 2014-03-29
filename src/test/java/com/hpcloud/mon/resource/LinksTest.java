3b23976ed412fdfb0967f028dc05d625f7a00889
Jonathan Halterman <jhalterman@gmail.com>
2014-03-28 17:16:14 -0700

    Test cleanup

--- src/test/java/com/hpcloud/mon/resource/LinksTest.java
+++ src/test/java/com/hpcloud/mon/resource/LinksTest.java
@@ -2,11 +2,7 @@ package com.hpcloud.mon.resource;
 
 import static org.testng.Assert.assertEquals;
 
-import java.nio.charset.Charset;
-
 import org.testng.annotations.Test;
-
-import com.google.common.hash.Hashing;
 
 /**
  * @author Jonathan Halterman
@@ -25,16 +21,5 @@ public class LinksTest {
     assertEquals(Links.prefixForHttps("http://abc123blah/blah/blah"), "http://abc123blah/blah/blah");
     assertEquals(Links.prefixForHttps("https://abc123blah/blah/blah"),
         "https://abc123blah/blah/blah");
-  }
-
-  public void foo() throws Exception {
-    byte[] foo = Hashing.sha1()
-        .newHasher()
-        .putString("foo", Charset.defaultCharset())
-        .putString("bar", Charset.defaultCharset())
-        .hash()
-        .asBytes();
-    String text1 = new String(foo, Charset.forName("US-ASCII"));
-    int i = 1;
   }
 }
