9ef2ab20ee1a9147232696f34af370bed59727fc
Jonathan Halterman <jhalterman@gmail.com>
2014-03-28 21:08:41 -0700

    DB cleanup, readying stubs

--- src/main/java/com/hpcloud/mon/infrastructure/persistence/StatisticRepositoryImpl.java
+++ src/main/java/com/hpcloud/mon/infrastructure/persistence/StatisticRepositoryImpl.java
@@ -3,7 +3,11 @@ package com.hpcloud.mon.infrastructure.persistence;
 import java.util.List;
 import java.util.Map;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 import org.joda.time.DateTime;
+import org.skife.jdbi.v2.DBI;
 
 import com.hpcloud.mon.domain.model.statistic.StatisticRepository;
 import com.hpcloud.util.stats.Statistic;
@@ -12,6 +16,13 @@ import com.hpcloud.util.stats.Statistic;
  * Vertica statistic repository implementation.
  */
 public class StatisticRepositoryImpl implements StatisticRepository {
+  private final DBI db;
+
+  @Inject
+  public StatisticRepositoryImpl(@Named("vertica") DBI db) {
+    this.db = db;
+  }
+
   @Override
   public List<Statistic> find(String tenantId, String name, Map<String, String> dimensions,
       DateTime startTime, DateTime endTime, List<String> statistics, int period) {
