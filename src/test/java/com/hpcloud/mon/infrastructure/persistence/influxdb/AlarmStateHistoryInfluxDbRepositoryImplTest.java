e96e64f01db806c8a87c29f104829f071f83a990
Deklan Dieterly <deklan.dieterly@hp.com>
2014-07-11 15:51:12 -0600

    Make tests use UTC dates.

--- src/test/java/com/hpcloud/mon/infrastructure/persistence/influxdb/AlarmStateHistoryInfluxDbRepositoryImplTest.java
+++ src/test/java/com/hpcloud/mon/infrastructure/persistence/influxdb/AlarmStateHistoryInfluxDbRepositoryImplTest.java
@@ -1,17 +1,15 @@
 package com.hpcloud.mon.infrastructure.persistence.influxdb;
 
 import com.hpcloud.mon.MonApiConfiguration;
-import com.hpcloud.mon.infrastructure.persistence.influxdb.AlarmStateHistoryInfluxDbRepositoryImpl;
-
 import org.influxdb.InfluxDB;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.skife.jdbi.v2.DBI;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import scala.actors.threadpool.Arrays;
 
 @Test
@@ -36,22 +34,20 @@ public class AlarmStateHistoryInfluxDbRepositoryImplTest {
 
   public void buildQueryForFindByIdTest() throws Exception {
 
-    String er =
-        "select alarm_id, old_state, new_state, reason, "
-            + "reason_data from alarm_state_history where tenant_id = 'tenant-id' and alarm_id = "
-            + "'alarm-id'";
-    String r =
-        this.alarmStateHistoryInfluxDBRepository.buildQueryForFindById("tenant-id", "alarm-id");
+    String er = "select alarm_id, old_state, new_state, reason, " +
+        "" + "reason_data from alarm_state_history where tenant_id = 'tenant-id' and alarm_id = "
+        + "'alarm-id'";
+    String r = this.alarmStateHistoryInfluxDBRepository.buildQueryForFindById("tenant-id",
+        "alarm-id");
 
     assert (er.equals(r));
 
   }
 
   public void buildTimePartTest() {
-    String er = " and time > 1388559600s and time < 1388559601s";
-    String r =
-        this.alarmStateHistoryInfluxDBRepository.buildTimePart(new DateTime(2014, 01, 01, 0, 0, 0),
-            new DateTime(2014, 01, 01, 0, 0, 1));
+    String er = " and time > 1388534400s and time < 1388534401s";
+    String r = this.alarmStateHistoryInfluxDBRepository.buildTimePart(new DateTime(2014, 01, 01,
+        0, 0, 0, DateTimeZone.UTC), new DateTime(2014, 01, 01, 0, 0, 1, DateTimeZone.UTC));
     assert (er.equals(r));
 
   }
@@ -59,21 +55,18 @@ public class AlarmStateHistoryInfluxDbRepositoryImplTest {
   @SuppressWarnings("unchecked")
   public void buildAlarmsPartTest() {
     String er = " and ( alarm_id = 'id-1'  or  alarm_id = 'id-2' )";
-    String r =
-        this.alarmStateHistoryInfluxDBRepository.buildAlarmsPart(Arrays.asList(new String[] {
-            "id-1", "id-2"}));
+    String r = this.alarmStateHistoryInfluxDBRepository.buildAlarmsPart(Arrays.asList(new
+        String[]{"id-1", "id-2"}));
     assert (er.equals(r));
   }
 
   public void buildQueryForFindTest() throws Exception {
-    String er =
-        "select alarm_id, old_state, new_state, reason, "
-            + "reason_data from alarm_state_history where tenant_id = 'tenant-id'  and time > "
-            + "1388559600s and time < 1388559601s  and ( alarm_id = 'id-1'  or  alarm_id = 'id-2' )";
-    String r =
-        this.alarmStateHistoryInfluxDBRepository.buildQueryForFind("tenant-id",
-            " and time > 1388559600s and time < 1388559601s", " and ( alarm_id = 'id-1'  or  "
-                + "alarm_id" + " = 'id-2' )");
+    String er = "select alarm_id, old_state, new_state, reason, " +
+        "" + "reason_data from alarm_state_history where tenant_id = 'tenant-id'  and time > " +
+        "1388559600s and time < 1388559601s  and ( alarm_id = 'id-1'  or  alarm_id = 'id-2' )";
+    String r = this.alarmStateHistoryInfluxDBRepository.buildQueryForFind("tenant-id",
+        " and time > 1388559600s and time < 1388559601s", " and ( alarm_id = 'id-1'  or  " +
+            "alarm_id" + " = 'id-2' )");
 
     assert (er.equals(r));
   }
