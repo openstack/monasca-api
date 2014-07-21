76ddef1b770f4f147931341e021d4bc9647b0f96
Deklan Dieterly <deklan.dieterly@hp.com>
2014-07-21 09:20:23 -0600

    Use InfluxDbConfiguration from mon-influxdb.
    Change-Id: Idb4982677192b6db89621e05d230b96929f3e792

--- src/main/java/com/hpcloud/mon/MonApiConfiguration.java
+++ src/main/java/com/hpcloud/mon/MonApiConfiguration.java
@@ -17,8 +17,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.hpcloud.messaging.kafka.KafkaConfiguration;
 import com.hpcloud.mon.infrastructure.middleware.MiddlewareConfiguration;
 import com.hpcloud.mon.infrastructure.persistence.DatabaseConfiguration;
-import com.hpcloud.mon.infrastructure.persistence.influxdb.InfluxDbConfig;
 
+import com.hpcloud.mon.infrastructure.persistence.influxdb.InfluxDbConfiguration;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
@@ -26,6 +26,7 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+
 
 public class MonApiConfiguration extends Configuration {
   @NotEmpty
@@ -52,9 +53,8 @@ public class MonApiConfiguration extends Configuration {
   @NotNull
   public MiddlewareConfiguration middleware;
   @Valid
-  public InfluxDbConfig influxDB;
+  public InfluxDbConfiguration influxDB;
   @Valid
-  @NotNull
   @JsonProperty
   public DatabaseConfiguration databaseConfiguration;
 
