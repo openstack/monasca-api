37d490c64262b1801799771fbf9616b35e725282
Deklan Dieterly <deklan.dieterly@hp.com>
2014-10-09 13:44:34 -0600

    Return metric list from InfluxDB as JSON
    Decode the metrics names from InfluxDB and return JSON.
    
    Change-Id: I36df355d3c673a34feaed2b49907885f6f9febf8

--- monasca/common/repositories/influxdb/metrics_repository.py
+++ monasca/common/repositories/influxdb/metrics_repository.py
@@ -4,51 +4,122 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import re
+import urllib
 
-from oslo.config import cfg
-from monasca.common.repositories import metrics_repository
-from monasca.common.repositories import exceptions
-from monasca.openstack.common import log
 from influxdb import InfluxDBClient
+from oslo.config import cfg
+
+from monasca.common.repositories import exceptions
+from monasca.common.repositories import metrics_repository
+from monasca.openstack.common import log
+
 
 LOG = log.getLogger(__name__)
 
-class MetricsRepository(metrics_repository.MetricsRepository):
 
+class MetricsRepository(metrics_repository.MetricsRepository):
     def __init__(self):
         try:
             self.conf = cfg.CONF
-            self.influxdb_client = InfluxDBClient(self.conf.influxdb.ip_address,
-                                                  self.conf.influxdb.port,
-                                                  self.conf.influxdb.user,
-                                                  self.conf.influxdb.password,
-                                                  self.conf.influxdb.database_name)
+            self.influxdb_client = InfluxDBClient(
+                self.conf.influxdb.ip_address, self.conf.influxdb.port,
+                self.conf.influxdb.user, self.conf.influxdb.password,
+                self.conf.influxdb.database_name)
+
+            # compile regex only once for efficiency
+            self._serie_name_reqex = re.compile(
+                '([^?&=]+)\?([^?&=]+)&([^?&=]+)(&[^?&=]+=[^?&=]+)*')
+            self._serie_name_tenant_id_region_regex = re.compile(
+                '[^?&=]+\?[^?&=]+&[^?&=]+')
+            self._serie_name_dimension_regex = re.compile('&[^?&=]+=[^?&=]+')
+            self._serie_name_dimension_parts_regex = re.compile(
+                '&([^?&=]+)=([^?&=]+)')
+
         except Exception as ex:
             LOG.exception()
             raise exceptions.RepositoryException(ex)
+
+    def _build_query(self, dimensions, name, tenant_id):
+
+        query = 'list series from /^'
+
+        if name:
+            query += urllib.quote(name.encode('utf8'),
+                                  safe='') + '\?' + urllib.quote(
+                tenant_id.encode('utf8'), safe='')
+        else:
+            query += '.+\?' + urllib.quote(tenant_id.encode('utf8'), safe='')
+
+        if dimensions:
+            for dimension_name, dimension_value in iter(
+                    sorted(dimensions.iteritems())):
+                query += '.*&'
+                query += urllib.quote(dimension_name.encode('utf8'), safe='')
+                query += '='
+                query += urllib.quote(dimension_value.encode('utf8'), safe='')
+
+        query += '/'
+
+        return query
 
     def list_metrics(self, tenant_id, name, dimensions):
         try:
-            # TODO: This code is just a placeholder right now. It actually returns metrics,
-            # TODO: but the implementation is not complete
-            #String serieNameRegex = buildSerieNameRegex(tenantId, name, dimensions);
 
-            #String query = String.format("list series /%1$s/", serieNameRegex);
-            #logger.debug("Query string: {}", query);
-            query = 'list series'
+            query = self._build_query(dimensions, name, tenant_id)
 
-            #List<Serie> result = this.influxDB.Query(this.config.influxDB.getName(), query,
-            #                                         TimeUnit.SECONDS);
             result = self.influxdb_client.query(query, 's')
-            #return buildMetricDefList(result);
-            return result
+
+            json_metric_list = self._decode_influxdb_serie_names(result)
+
+            return json_metric_list
+
         except Exception as ex:
             LOG.exception()
             raise exceptions.RepositoryException(ex)
+
+    def _decode_influxdb_serie_names(self, series_names):
+
+        json_metric_list = []
+        serie_names_list_list = series_names[0]['points']
+        for serie_name_list in serie_names_list_list:
+            serie_name = serie_name_list[1]
+            match = self._serie_name_reqex.match(serie_name)
+            if match:
+                name = urllib.unquote(match.group(1))
+                # throw tenant_id (group 2) and region (group 3) away
+
+                # only returns the last match. we need all dimensions.
+                dimensions = match.group(4)
+                if dimensions:
+                    # remove the name, tenant_id, and region; just
+                    # dimensions remain
+                    dimensions_part = \
+                        self._serie_name_tenant_id_region_regex.sub(
+                        '', serie_name)
+                    dimensions = {}
+                    dimension_list = self._serie_name_dimension_regex.findall(
+                        dimensions_part)
+                    for dimension in dimension_list:
+                        match = self._serie_name_dimension_parts_regex.match(
+                            dimension)
+                        dimension_name = urllib.unquote(match.group(1))
+                        dimension_value = urllib.unquote(match.group(2))
+                        dimensions[dimension_name.encode(
+                            'utf8')] = dimension_value.encode('utf8')
+                    metric = {"name": name.encode('utf8'),
+                              "dimensions": dimensions}
+                else:
+                    metric = {"name": name.encode('utf8')}
+
+                json_metric_list.append(metric)
+
+        return json_metric_list
+
--- monasca/v2/reference/metrics.py
+++ monasca/v2/reference/metrics.py
@@ -4,7 +4,7 @@
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
@@ -13,9 +13,12 @@
 # under the License.
 
 import json
+import re
+import urllib
 
 import falcon
 from oslo.config import cfg
+from stevedore import driver
 
 from monasca.openstack.common import log
 from monasca.api import monasca_api_v2
@@ -24,10 +27,10 @@ from monasca.common.messaging import exceptions as message_queue_exceptions
 from monasca.common.messaging.message_formats import metrics_transform_factory
 from monasca.v2.common import utils
 from monasca.v2.common.schemas import exceptions as schemas_exceptions
-from monasca.v2.common.schemas import metrics_request_body_schema as schemas_metrics
+from monasca.v2.common.schemas import \
+    metrics_request_body_schema as schemas_metrics
 from monasca.v2.reference import helpers
 
-from stevedore import driver
 
 LOG = log.getLogger(__name__)
 
@@ -36,30 +39,29 @@ class Metrics(monasca_api_v2.V2API):
     def __init__(self, global_conf):
         super(Metrics, self).__init__(global_conf)
         self._region = cfg.CONF.region
-        self._default_authorized_roles = cfg.CONF.security.default_authorized_roles
-        self._delegate_authorized_roles = cfg.CONF.security.delegate_authorized_roles
-        self._post_metrics_authorized_roles = cfg.CONF.security.default_authorized_roles + \
-                                              cfg.CONF.security.agent_authorized_roles
-        self._metrics_transform = metrics_transform_factory.create_metrics_transform()
+        self._default_authorized_roles = \
+            cfg.CONF.security.default_authorized_roles
+        self._delegate_authorized_roles = \
+            cfg.CONF.security.delegate_authorized_roles
+        self._post_metrics_authorized_roles = \
+            cfg.CONF.security.default_authorized_roles + \
+            cfg.CONF.security.agent_authorized_roles
+        self._metrics_transform = \
+            metrics_transform_factory.create_metrics_transform()
         self._init_message_queue()
         self._init_metrics_repo()
 
     def _init_message_queue(self):
-        mgr = driver.DriverManager(
-            namespace = 'monasca.messaging',
-            name = cfg.CONF.messaging.driver,
-            invoke_on_load=True,
-            invoke_args=(['metrics'])
-        )
+        mgr = driver.DriverManager(namespace='monasca.messaging',
+                                   name=cfg.CONF.messaging.driver,
+                                   invoke_on_load=True,
+                                   invoke_args=(['metrics']))
         self._message_queue = mgr.driver
 
     def _init_metrics_repo(self):
-        mgr = driver.DriverManager(
-            namespace = 'monasca.repositories',
-            name = cfg.CONF.repositories.metrics_driver,
-            invoke_on_load=True,
-            invoke_args=()
-        )
+        mgr = driver.DriverManager(namespace='monasca.repositories',
+                                   name=cfg.CONF.repositories.metrics_driver,
+                                   invoke_on_load=True, invoke_args=())
         self._metrics_repo = mgr.driver
 
     def _read_metrics(self, req):
@@ -75,7 +77,8 @@ class Metrics(monasca_api_v2.V2API):
             return json_msg
         except ValueError as ex:
             LOG.debug(ex)
-            raise falcon.HTTPBadRequest('Bad request', 'Request body is not valid JSON')
+            raise falcon.HTTPBadRequest('Bad request',
+                                        'Request body is not valid JSON')
 
     def _validate_metrics(self, metrics):
         '''
@@ -95,13 +98,15 @@ class Metrics(monasca_api_v2.V2API):
         :param metrics: A metric object or array of metrics objects.
         :raises: falcon.HTTPServiceUnavailable:
         '''
+
         def _send_metric(metric):
             try:
                 str_msg = json.dumps(metric, default=utils.date_handler)
                 self._message_queue.send_message(str_msg)
             except message_queue_exceptions.MessageQueueException as ex:
                 LOG.exception(ex)
-                raise falcon.HTTPServiceUnavailable('Service unavailable', ex.message)
+                raise falcon.HTTPServiceUnavailable('Service unavailable',
+                                                    ex.message)
 
         if isinstance(metrics, list):
             for metric in metrics:
@@ -117,22 +122,26 @@ class Metrics(monasca_api_v2.V2API):
         :param dimensions:
         :raises falcon.HTTPServiceUnavailable:
         '''
+
         try:
-            result = self._metrics_repo.list_metrics(tenant_id, name, dimensions)
-            # TODO: Format response body correctly. Currently just returning what is returned by the metrics repository.
-            return result
+            return self._metrics_repo.list_metrics(tenant_id, name,
+                                                     dimensions)
         except Exception as ex:
-            log.exception()
-            raise falcon.HTTPServiceUnavailable('Service unavailable', ex.message)
+            LOG.exception(ex)
+            raise falcon.HTTPServiceUnavailable('Service unavailable',
+                                                ex.message)
 
     @resource_api.Restify('/v2.0/metrics/', method='post')
     def do_post_metrics(self, req, res):
         helpers.validate_json_content_type(req)
-        helpers.validate_authorization(req, self._post_metrics_authorized_roles)
+        helpers.validate_authorization(req,
+                                       self._post_metrics_authorized_roles)
         metrics = self._read_metrics(req)
         self._validate_metrics(metrics)
-        tenant_id = helpers.get_cross_tenant_or_tenant_id(req, self._delegate_authorized_roles)
-        transformed_metrics = self._metrics_transform(metrics, tenant_id, self._region)
+        tenant_id = helpers.get_cross_tenant_or_tenant_id(req,
+                                                          self._delegate_authorized_roles)
+        transformed_metrics = self._metrics_transform(metrics, tenant_id,
+                                                      self._region)
         self._send_metrics(transformed_metrics)
         res.status = falcon.HTTP_204
 
@@ -145,5 +154,5 @@ class Metrics(monasca_api_v2.V2API):
         dimensions = helpers.get_query_dimensions(req)
         helpers.validate_query_dimensions(dimensions)
         result = self._list_metrics(tenant_id, name, dimensions)
-        res.body =  json.dumps(result)
+        res.body = json.dumps(result)
         res.status = falcon.HTTP_200
