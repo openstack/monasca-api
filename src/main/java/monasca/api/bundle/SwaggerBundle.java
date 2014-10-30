9118a9c72b322a13a7ac815583860fcf8b16dcf1
Craig Bryant <craig.bryant@hp.com>
2014-10-29 21:26:40 -0600

    Fix Swagger initialization
    It used to assume if /var/lib/cloud existed, then it was running
    in EC2 but that caused issues with ansible or trusty. Now it
    always assumes it is not running in EC2
    
    Change-Id: I7cb93ac2184ade9c40222050b07069144affc491

--- src/main/java/monasca/api/bundle/SwaggerBundle.java
+++ src/main/java/monasca/api/bundle/SwaggerBundle.java
@@ -22,12 +22,7 @@ import io.dropwizard.server.ServerFactory;
 import io.dropwizard.server.SimpleServerFactory;
 import io.dropwizard.setup.Environment;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -80,19 +75,8 @@ public class SwaggerBundle extends AssetsBundle {
   private static String getSwaggerBasePath(Configuration configuration) throws IOException {
     String host;
 
-    if (!new File("/var/lib/cloud/").exists()) {
-      LOGGER.info("/var/lib/cloud does not exist, assuming that we are running locally");
-      host = "localhost";
-    } else {
-      HttpURLConnection urlConnection =
-          (HttpURLConnection) new URL("http://169.254.169.254/latest/meta-data/public-hostname/")
-              .openConnection();
-      urlConnection.setRequestMethod("GET");
-      try (BufferedReader reader =
-          new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
-        host = reader.readLine();
-      }
-    }
+    host = "localhost";
+    LOGGER.info("Assume we are running locally");
 
     ServerFactory serverFactory = configuration.getServerFactory();
     HttpConnectorFactory httpConnectorFactory = null;
