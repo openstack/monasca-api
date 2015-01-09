1c3f4465c5900c7dad04060b2a8277922cf1f2fe
Craig Bryant <craig.bryant@hp.com>
2015-01-09 08:30:31 -0700

    Fix metric name and dimension values matching
    Previously, the regular expression for matching metric names and
    dimension values in influxdb series names was only requiring the
    start of the metric name and dimension values to match the given
    values. Now, the entire metric name and dimension value must match
    the given values.
    
    Add tests
    
    Change-Id: I0a41b5051b7d385c86a2927ee3418e1757ca6e11

--- src/main/java/monasca/api/infrastructure/persistence/influxdb/Utils.java
+++ src/main/java/monasca/api/infrastructure/persistence/influxdb/Utils.java
@@ -91,6 +91,7 @@ final class Utils {
     if (name != null) {
       regex.append("&");
       regex.append(urlEncodeUTF8(name));
+      regex.append("(&|$)");
     }
 
     // Dimensions are optional.
@@ -100,10 +101,11 @@ final class Utils {
       final TreeSet<Dimension> dimSortedSet = buildSortedDimSet(dimensions);
 
       for (final Dimension dim : dimSortedSet) {
-        regex.append(".*&");
+        regex.append("(.*&)*");
         regex.append(urlEncodeUTF8(dim.name));
         regex.append("=");
         regex.append(urlEncodeUTF8(dim.value));
+        regex.append("(&|$)");
       }
     }
     return regex.toString();
