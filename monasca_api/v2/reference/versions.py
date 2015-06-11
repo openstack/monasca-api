03df901ae2fdfec079a1e75ea2aa7266278a8f81
SamKirsch10 <sam.kirsch@hp.com>
2015-06-11 13:12:49 -0600

    added version for restapi and fixed requirements to = 0.2
    Change-Id: I499da5caea26ac104ceda896cb795ee9a595def9

--- monasca_api/v2/reference/versions.py
+++ monasca_api/v2/reference/versions.py
@@ -11,12 +11,25 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import json
 
+import falcon
 
 from monasca_api.api import versions_api
 from monasca_api.openstack.common import log
 
 LOG = log.getLogger(__name__)
+VERSIONS = {
+    'v2.0': {
+        'id': 'v2.0',
+        'links': [{
+            'rel': 'self',
+            'href': ''
+        }],
+        'status': 'CURRENT',
+        'updated': "2013-03-06T00:00:00Z"
+    }
+}
 
 
 class Versions(versions_api.VersionsAPI):
@@ -24,5 +37,27 @@ class Versions(versions_api.VersionsAPI):
     def __init__(self):
         super(Versions, self).__init__()
 
-    def on_get(self, req, res, id):
-        res.status = '501 Not Implemented'
+    def on_get(self, req, res, version_id=None):
+        result = {
+            'links': [{
+                'rel': 'self',
+                'href': req.uri.decode('utf8')
+            }],
+            'elements': []
+        }
+        if version_id is None:
+            for version in VERSIONS:
+                VERSIONS[version]['links'][0]['href'] = (
+                    req.uri.decode('utf8') + version)
+                result['elements'].append(VERSIONS[version])
+            res.body = json.dumps(result)
+            res.status = falcon.HTTP_200
+        else:
+            if version_id in VERSIONS:
+                VERSIONS[version_id]['links'][0]['href'] = (
+                    req.uri.decode('utf8'))
+                res.body = json.dumps(VERSIONS[version_id])
+                res.status = falcon.HTTP_200
+            else:
+                res.body = 'Invalid Version ID'
+                res.status = falcon.HTTP_400
