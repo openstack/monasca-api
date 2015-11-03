6d36f4791a562b26fedc07fb389f3a3901b0a013
kaiyan-sheng <kaiyan.sheng@hp.com>
2015-11-03 10:42:52 -0700

    Moved tempest tests back to monasca_tempest_tests folder
    Change-Id: I76cae8f3281fbb8c143115f2e61fa4fb7ecc3d72

--- monasca_tempest_tests/tests/api/base.py
+++ monasca_tempest_tests/tests/api/base.py
@@ -30,40 +30,38 @@ class BaseMonascaTest(tempest.test.BaseTestCase):
     @classmethod
     def resource_setup(cls):
         super(BaseMonascaTest, cls).resource_setup()
-        # cls.os = clients.Manager()
-        # cls.monasca_client = cls.os.monasca_client
-        pass
+        cls.os = clients.Manager()
+        cls.monasca_client = cls.os.monasca_client
 
     @staticmethod
     def cleanup_resources(method, list_of_ids):
-        # for resource_id in list_of_ids:
-        #   try:
-        #        method(resource_id)
-        #    except exceptions.NotFound:
-        #       pass
-        pass
+        for resource_id in list_of_ids:
+            try:
+                method(resource_id)
+            except exceptions.NotFound:
+                pass
 
     @classmethod
     def resource_cleanup(cls):
         super(BaseMonascaTest, cls).resource_cleanup()
-#        resp, response_body = cls.monasca_client.list_alarm_definitions()
-#        elements = response_body['elements']
-#        if elements:
-#            for element in elements:
-#                id = element['id']
-#                cls.monasca_client.delete_alarm_definition(id)
+        resp, response_body = cls.monasca_client.list_alarm_definitions()
+        elements = response_body['elements']
+        if elements:
+            for element in elements:
+                id = element['id']
+                cls.monasca_client.delete_alarm_definition(id)
 
-#        resp, response_body = cls.monasca_client.list_notification_methods()
-#        elements = response_body['elements']
-#        if elements:
-#            for element in elements:
-#                id = element['id']
-#               cls.monasca_client.delete_notification_method(id)
+        resp, response_body = cls.monasca_client.list_notification_methods()
+        elements = response_body['elements']
+        if elements:
+            for element in elements:
+                id = element['id']
+                cls.monasca_client.delete_notification_method(id)
 
-#        resp, response_body = cls.monasca_client.list_alarms()
-#        elements = response_body['elements']
-#        if elements:
-#            for element in elements:
-#                id = element['id']
-#                cls.monasca_client.delete_alarm(id)
-        pass
+        resp, response_body = cls.monasca_client.list_alarms()
+        elements = response_body['elements']
+        if elements:
+            for element in elements:
+                id = element['id']
+                cls.monasca_client.delete_alarm(id)
+
