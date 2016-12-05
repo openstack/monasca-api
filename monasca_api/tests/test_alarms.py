dae25f519d902ef06d9cbd6c2be95f42041c0ecf
liyingjun <yingjun.li@kylin-cloud.com>
2016-12-05 21:00:08 +0800

    Fix UnicodeEncodeError for alarm definition
    Alarm definition created with Chinese in description, when trying to get
    alarm definition, UnicodeEncodeError raises. Decode description to unicode
    when the alarm definition is created.
    
    Change-Id: Ic5fa1479783a2bafa6c7d16e7837b46bf88d9e7d
    Closes-bug: #1647342

--- monasca_api/tests/test_alarms.py
+++ monasca_api/tests/test_alarms.py
@@ -331,7 +331,7 @@ class TestAlarmDefinition(AlarmTestBase):
         self.alarm_def_repo_mock.return_value.update_or_patch_alarm_definition.return_value = (
             {u'alarm_actions': [],
              u'ok_actions': [],
-             u'description': u'Non-ASCII character: \u2603'.encode('utf-8'),
+             u'description': u'Non-ASCII character: \u2603',
              u'match_by': u'hostname',
              u'name': u'Test Alarm',
              u'actions_enabled': True,
@@ -411,7 +411,7 @@ class TestAlarmDefinition(AlarmTestBase):
         self.alarm_def_repo_mock.return_value.update_or_patch_alarm_definition.return_value = (
             {u'alarm_actions': [],
              u'ok_actions': [],
-             u'description': u'Non-ASCII character: \u2603'.encode('utf-8'),
+             u'description': u'Non-ASCII character: \u2603',
              u'match_by': u'hostname',
              u'name': u'Test Alarm',
              u'actions_enabled': True,
@@ -501,7 +501,9 @@ class TestAlarmDefinition(AlarmTestBase):
         self.alarm_def_repo_mock.return_value.get_alarm_definition.return_value = {
             'alarm_actions': None,
             'ok_actions': None,
-            'description': b'Non-ASCII character: \xe2\x98\x83',
+            # The description field was decoded to unicode when the
+            # alarm_definition was created.
+            'description': u'Non-ASCII character: \u2603',
             'match_by': u'hostname',
             'name': u'Test Alarm',
             'actions_enabled': 1,
