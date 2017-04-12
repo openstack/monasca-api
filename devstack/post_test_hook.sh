65e649abe033720eff94301d1247571e8caf7731
Witold Bedyk <witold.bedyk@est.fujitsu.com>
2017-04-12 17:00:13 +0200

    Add bindep.txt
    (cherry picked from commit bcee24b323a57a06c352d7365167004722e0ea12)
    (cherry picked from commit Iec6e82a6ddc8ce4e405196509777c029139b5025)
    (cherry picked from commit Ib25e7870b6340403be8e6d49ded8a19b7e062186)
    
    Change-Id: I04d238d898caf89f22fa08576850740ec7ee98b6

--- devstack/post_test_hook.sh
+++ devstack/post_test_hook.sh
@@ -15,24 +15,23 @@
 # limitations under the License.
 #
 
-(cd $BASE/new/tempest/; sudo virtualenv .venv)
-source $BASE/new/tempest/.venv/bin/activate
+source $BASE/new/devstack/stackrc
+CONSTRAINTS="-c $REQUIREMENTS_DIR/upper-constraints.txt"
+REQS="$CONSTRAINTS -r requirements.txt -r test-requirements.txt"
 
-(cd $BASE/new/tempest/; sudo pip install -r requirements.txt -r test-requirements.txt)
-sudo pip install nose
-sudo pip install numpy
+(cd $BASE/new/tempest/; sudo -EH pip install $REQS)
 
-(cd $BASE/new/tempest/; sudo oslo-config-generator --config-file  tempest/cmd/config-generator.tempest.conf  --output-file etc/tempest.conf)
-(cd $BASE/new/; sudo sh -c 'cat monasca-api/devstack/files/tempest/tempest.conf >> tempest/etc/tempest.conf')
+(cd $BASE/new/tempest/; sudo -EH oslo-config-generator --config-file  tempest/cmd/config-generator.tempest.conf  --output-file etc/tempest.conf)
+(cd $BASE/new/; sudo -EH sh -c 'cat monasca-api/devstack/files/tempest/tempest.conf >> tempest/etc/tempest.conf')
 
 sudo cp $BASE/new/tempest/etc/logging.conf.sample $BASE/new/tempest/etc/logging.conf
 
-(cd $BASE/new/monasca-api/; sudo pip install -r requirements.txt -r test-requirements.txt)
-(cd $BASE/new/monasca-api/; sudo python setup.py install)
+(cd $BASE/new/monasca-api/; sudo -EH pip install $REQS)
+(cd $BASE/new/monasca-api/; sudo -EH python setup.py install)
 
-(cd $BASE/new/tempest/; sudo testr init)
+(cd $BASE/new/tempest/; sudo -EH testr init)
 
-(cd $BASE/new/tempest/; sudo sh -c 'testr list-tests monasca_tempest_tests > monasca_tempest_tests')
-(cd $BASE/new/tempest/; sudo sh -c 'cat monasca_tempest_tests')
-(cd $BASE/new/tempest/; sudo sh -c 'cat monasca_tempest_tests | grep gate > monasca_tempest_tests_gate')
-(cd $BASE/new/tempest/; sudo sh -c 'testr run --subunit --load-list=monasca_tempest_tests_gate | subunit-trace --fails')
+(cd $BASE/new/tempest/; sudo -EH sh -c 'testr list-tests monasca_tempest_tests > monasca_tempest_tests')
+(cd $BASE/new/tempest/; sudo -EH sh -c 'cat monasca_tempest_tests')
+(cd $BASE/new/tempest/; sudo -EH sh -c 'cat monasca_tempest_tests | grep gate > monasca_tempest_tests_gate')
+(cd $BASE/new/tempest/; sudo -EH sh -c 'testr run --subunit --load-list=monasca_tempest_tests_gate | subunit-trace --fails')
